2de7abb704bc3df886edb96e72f4b4bc3beb9801
AnishaLamichhane <ansalamichhane111@gmail.com>
2022-02-01 12:19:38 +0545

    finishing UI for SignUpViewController

---
+++ Recipe-App/Controllers/SignUpViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpViewController.swift
+//  Recipe-App
+//
+//  Created by Anisha Lamichhane on 2/1/22.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
