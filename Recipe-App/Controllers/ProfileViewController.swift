a4244a40d18c3fd804a547dfa3c180ee6eb40a4e
AnishaLamichhane <ansalamichhane111@gmail.com>
2022-03-21 22:42:45 +0545

    adding navigation bar

--- Recipe-App/Controllers/ProfileViewController.swift
+++ Recipe-App/Controllers/ProfileViewController.swift
@@ -11,6 +11,7 @@ class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.setHidesBackButton(true, animated: true)
 
         // Do any additional setup after loading the view.
     }
