9d14739d3afdd8e3417344c9f6313bc7d1eacbc7
AnishaLamichhane <ansalamichhane111@gmail.com>
2022-03-24 15:24:21 +0545

    shadow box effect

--- Recipe-App/Controllers/HomeTableViewController.swift
+++ Recipe-App/Controllers/HomeTableViewController.swift
@@ -22,7 +22,7 @@ class HomeTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return 4
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
---
+++ Recipe-App/Custom/ShadowBoxView.swift
@@ -0,0 +1,25 @@
+//
+//  ShadowBoxView.swift
+//  Recipe-App
+//
+//  Created by Anisha Lamichhane on 3/24/22.
+//
+
+import UIKit
+
+class ShadowBoxView: UIView {
+    override func draw(_ rect: CGRect) {
+        applyShadow()
+    }
+    func applyShadow(){
+        self.layer.shadowColor = UIColor.darkGray.cgColor
+        self.layer.shadowOpacity = 0.5
+        self.layer.shadowOffset = CGSize(width: 0, height: 8)
+        self.layer.shadowRadius = 8
+        let radii = CGSize(width: 8, height: 8)
+        self.layer.shadowPath = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: .allCorners, cornerRadii: radii).cgPath
+        self.layer.shouldRasterize = true
+        self.layer.rasterizationScale = UIScreen.main.scale
+    }
+    
+}
