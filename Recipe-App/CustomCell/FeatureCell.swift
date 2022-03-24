23cebb72968d7c3ecf55f67d02727e630ae00850
AnishaLamichhane <ansalamichhane111@gmail.com>
2022-03-24 15:41:30 +0545

    shadow box

---
+++ Recipe-App/CustomCell/FeatureCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeatureCell.swift
+//  Recipe-App
+//
+//  Created by Anisha Lamichhane on 3/24/22.
+//
+
+import UIKit
+
+class FeatureCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
