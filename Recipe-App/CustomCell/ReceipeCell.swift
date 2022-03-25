e71fcb765a284e9fe7e392d88911254738280c94
AnishaLamichhane <ansalamichhane111@gmail.com>
2022-03-25 23:02:20 +0545

    shadow box

--- Recipe-App/CustomCell/ReceipeCell.swift
+++ Recipe-App/CustomCell/ReceipeCell.swift
@@ -10,6 +10,8 @@ import UIKit
 class ReceipeCell: UITableViewCell {
 
     override func awakeFromNib() {
+        //
+        //
         super.awakeFromNib()
         selectionStyle = .none
     }
