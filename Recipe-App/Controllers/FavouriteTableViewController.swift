625ca7248d21075c7410e649bff9761caf36a32f
AnishaLamichhane <ansalamichhane111@gmail.com>
2022-03-29 21:22:57 +0545

    setting favouriteTableViewController

--- Recipe-App/Controllers/FavouriteTableViewController.swift
+++ Recipe-App/Controllers/FavouriteTableViewController.swift
@@ -11,24 +11,22 @@ class FavouriteTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationItem.setHidesBackButton(true, animated: true)
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        let receipeCell = UINib.init(nibName: "ReceipeCell", bundle: nil)
+        tableView.register(receipeCell, forCellReuseIdentifier: "ReceipeCell")
+       
     }
 
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 5
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ReceipeCell", for: indexPath) as! ReceipeCell
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 260
     }
 
     override func viewWillAppear(_ animated: Bool) {
